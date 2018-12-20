2293b3ab83573e8e8fe41bb4246808384186e029
dalongrong <1141591465@qq.com>
2018-12-20 16:48:38 +0800

    add  markdown demo

---
+++ content.md
@@ -0,0 +1,14 @@
+# fuserlogin
+
+## usernames
+
+* dalongs
+* demoapp
+
+## print mark html
+
+ ```diff
+ + userlogin=(name,age)=>{console.log(name,age)}
+ + return userlogin
+
+ ```
--- m.js
+++ m.js
@@ -1,1 +1,23 @@
-console.log("markdown demo")
+require('raw-module-require-hook')({
+    extensions: ['txt', 'md'],
+  });
+var content = require("./content.md");
+var myMarked = require('marked');
+myMarked.setOptions({
+  renderer: new myMarked.Renderer(),
+  highlight: function(code) {
+    return require('highlight.js').highlightAuto(code).value;
+  },
+  code: true,
+  pedantic: false,
+  gfm: true,
+  tables: true,
+  breaks: false,
+  sanitize: false,
+  smartLists: true,
+  smartypants: false,
+  xhtml: false
+});
+
+// Compile
+console.log(myMarked(content));
